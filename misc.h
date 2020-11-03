0fb29696cca41b599710e78b7832268293f38f42
Yoshiaki JITSUKAWA <unicodon@gmail.com>
2020-11-03 09:55:29 +0900

    Update misc.h

--- misc.h
+++ misc.h
@@ -23,3 +23,21 @@ bool isprim(long long A)
 	return true;
 }
 
+
+template<typename T> bool chmax(T& a, const T& b)
+{
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+template<typename T> bool chmin(T& a, const T& b)
+{
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
