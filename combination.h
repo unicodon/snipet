43290d4d4d115d66244fe11a4c5c83d8ac349a9f
Yoshiaki JITSUKAWA <unicodon@gmail.com>
2020-11-23 21:12:10 +0900

    Update combination.h

--- combination.h
+++ combination.h
@@ -26,3 +26,53 @@ private:
 	llvec inv;
 //	llvec fact_inv;
 };
+
+template<ll P>
+class Combination {
+public:
+	Combination() {
+		m_fact.resize(16);
+		m_inv.resize(16);
+		m_fact_inv.resize(16);
+		m_fact[0] = m_fact[1] = 1;
+		m_inv[0] = m_inv[1] = 1;
+		m_fact_inv[1] = m_fact_inv[1] = 1;
+	}
+
+	ll fact(ll x)
+	{
+		if (m_fact.size() < x + 1) m_fact.resize(x + 1);
+		if (m_fact[x]) return m_fact[x];
+		return m_fact[x] = mod(x * fact(x-1));
+	}
+
+	ll inv(ll x)
+	{
+		if (m_inv.size() < x + 1) m_inv.resize(x + 1);
+		if (m_inv[x]) return m_inv[x];
+		return m_inv[x] = P - inv(P % x) * (P / x) % P;
+	}
+
+	ll fact_inv(ll x)
+	{
+		if (m_fact_inv.size() < x + 1) m_fact_inv.resize(x + 1);
+		if (m_fact_inv[x]) return m_fact_inv[x];
+		return m_fact_inv[x] = mod(fact_inv(x - 1) * inv(x));
+	}
+
+	ll mod(ll x)
+	{
+		return x % P;
+	}
+
+	ll operator()(ll n, ll r)
+	{
+		return mod(fact(n) * mod(fact_inv(r) * fact_inv(n - r)));
+	}
+
+private:
+	llvec m_fact;
+	llvec m_inv;
+	llvec m_fact_inv;
+};
+
