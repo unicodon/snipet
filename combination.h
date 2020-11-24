b2d7415ea2ee62cd283484e7516d3c4d2d7ecb49
Yoshiaki JITSUKAWA <unicodon@gmail.com>
2020-11-24 13:41:03 +0900

    Update combination.h

--- combination.h
+++ combination.h
@@ -2,10 +2,8 @@ class Combination1 {
 public:
 	Combination1(ll P, ll N) : P(P), N(N), comb(N+1), inv(N+1) /*, fact_inv(N+1)*/ {
 		inv[0] = inv[1] = 1;
-		// fact_inv[1] = fact_inv[1] = 1;
 		for (ll i = 2; i < N; i++) {
 			inv[i] = P - inv[P % i] * (P / i) % P;
-			// fact_inv[i] = fact_inv[i - 1] * inv[i] % P;
 		}
 		comb[0] = 1;
 		for (ll i = 1; i < N; i++) {
@@ -24,7 +22,6 @@ private:
 	ll N;
 	llvec comb;
 	llvec inv;
-//	llvec fact_inv;
 };
 
 template<ll P>
@@ -36,7 +33,7 @@ public:
 		m_fact_inv.resize(16);
 		m_fact[0] = m_fact[1] = 1;
 		m_inv[0] = m_inv[1] = 1;
-		m_fact_inv[1] = m_fact_inv[1] = 1;
+		m_fact_inv[0] = m_fact_inv[1] = 1;
 	}
 
 	ll fact(ll x)
@@ -75,4 +72,3 @@ private:
 	llvec m_inv;
 	llvec m_fact_inv;
 };
-
