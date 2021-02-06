de478b0c8e5eb6fd11e8ca6dbd0a2238943487b7
Yoshiaki JITSUKAWA <unicodon@gmail.com>
2021-02-06 17:16:03 +0900

    Update common.h

--- common.h
+++ common.h
@@ -21,6 +21,7 @@
 using namespace std;
 
 #define REP(i,n) for (decltype(n) i = 0; i < n; i++)
+#define THE_MOD 1'000'000'007
 
 using ll = long long;
 using ivec = vector<int>;
@@ -32,10 +33,13 @@ using llpair = pair<ll, ll>;
 #define umap unordered_map
 #define uset unordered_set
 
+template<typename T> ostream& operator <<(ostream& os, const vector<T> &v) { auto n = v.size(); REP(i,n) os << (i ? " " : "") << v[i]; return os; }
+template<typename T> istream& operator >>(istream& is, vector<T> &v) { for(auto &e : v) is >> e; return is; }
+template<typename T, typename U> ostream& operator <<(ostream& os, const pair<T, U> &p) { os << p.first << " " << p.second; return os; }
+template<typename T, typename U> istream& operator >>(istream& is, pair<T, U> &p) { is >> p.first >> p.second; return is; }
+
 void in() {} template <typename T, typename... Args> void in(T& t, Args& ...args) { cin >> t; in(args...); }
 void out() { cout << endl; } template <typename T, typename... Args> void out(const T& t, const Args& ...args) { cout << t; if (sizeof...(args)) cout << " "; out(args...); }
-
-#define THE_MOD 1'000'000'007
 
 constexpr ll LLINF = numeric_limits<ll>::max();
 
